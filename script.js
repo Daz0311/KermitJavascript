fef51fcc41ecb54881c1618579223120c482866f
DT-08\Admin <dario.tarasco@remira.com>
2024-08-08 12:28:45 +0200

    sasa

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// OPERAZIONI DA FARE A INIZIO PAGINA
+// - OPERAZIONI DA FARE AD AVVIO PAGINA
 
 // Recupero gli elementi di interesse nella pagina
 const button = document.querySelector('button');
@@ -6,38 +6,110 @@ const inputField = document.querySelector('input');
 const todoList = document.querySelector('.todo-list');
 const emptyListMessage = document.querySelector('.empty-list-message');
 
+// Creo una chiave per il local storage
+const STORAGE_KEY = '__bool_todo__';
+
 // Preparo una lista attività
-let activities = ["Take Kermit", "Love Kermit", "Hug Kermit"];
+let activities = [];
+
+// Controllo se per caso c'erano delle attività nel local storage
+const storage = localStorage.getItem(STORAGE_KEY);
+
+if(storage){
+    activities = JSON.parse(storage);
+}
 
 // Chiedo a JS di decidere cosa mostrare
 showContent();
 
-// FUNZIONI
+// - OPERAZIONI DINAMICHE
+// Reagisco al click del bottone
+button.addEventListener('click', function() {
+// chiedo di aggiungere attività
+addActivity()
+});
+
+
+// - FUNZIONI
 // Funzione che decide cosa mostrare in pagina
 function showContent(){
     // innanzitutto pulisco tutto
     todoList.innerText = "";
     emptyListMessage.innerText = "";
-}
 
-if(activities.length > 0){
 
     // Se c'è almeno un'attività...
-    // mostra le attività
-    //per ciascuna attività...
+if(activities.length > 0){
+    // per ciascuna attività... 
     activities.forEach(function(activity) {
-        console.log( `
-               <li class="todo-item">
-                <div class="todo-check">
-                <img src="images/checked.png" alt="check icon" width="20px">
-            </div>
-                <p class="todo-text">${activity}</p>
-            </li>` )
+    // Crea un template HTML
+        const template = createActivityTemplate(activity)
+        
+    // Inseriscilo in pagina
+    todoList.innerHTML += template;
     });
-    
+
+  // Rendi cliccabili tutti i check
+  makeCheckClickable();
+
+
 } else {
     // ALTRIMENTI
     //mostra il messaggio di lista vuota
-    emptyListMessage.innerText = "Niente da fare per Kermit"
+    emptyListMessage.innerText = "Nothing to be done for Kermit."
 }
-    
+}
+
+// Funzione per rendere i check cliccabili
+function makeCheckClickable(){
+      // Cerca tutti i check e fa sì che siano clicabili
+      const checks = document.querySelectorAll(".todo-check");
+      // Per ognuno dei check
+      checks.forEach(function(check, index){
+          //Aggiungi una reazione al click
+          check.addEventListener("click", function(){
+              //Rimuovi l'elemento dalla lista
+              activities.splice(index , 1)
+
+              // Aggiorna anche il localStorage
+              localStorage.setItem(STORAGE_KEY, JSON.stringify(activities));
+              
+              // Aggiorna la lista in pagina
+              showContent();
+          });
+      })
+}
+
+// Funzione per aggiungere attività
+function addActivity() {
+// Recupero il testo nel campo
+const newActivity = inputField.value.trim();
+
+// Se il campo non è vuoto...
+if(newActivity.length > 0){
+// Aggiungo l'attività alla lista
+activities.push(newActivity);
+
+// Aggiorna lo storage
+localStorage.setItem(STORAGE_KEY, JSON.stringify(activities));
+
+// Ora, decidi cosa mostrare
+showContent();
+
+// Svuoto il campo
+inputField.value = "";
+}
+}
+
+// Funzione che crea un template HTMl per un'attività
+function createActivityTemplate(activity){
+    //restituisci questo pezzo di HTML
+    return `
+       <li class="todo-item">
+        <div class="todo-check">
+        <img src="images/checked.png" alt="check icon" width="20px">
+    </div>
+        <p class="todo-text">${activity}</p>
+    </li>
+    `;
+}
